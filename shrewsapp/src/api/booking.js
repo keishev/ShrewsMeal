f4d4655e9b8e323d83ec30d76f242abf47494f75
Keisha Shevila <keisha.als118@gmail.com>
2025-03-13 23:09:57 +0800

    Updated the booking api

--- shrewsapp/src/api/booking.js
+++ shrewsapp/src/api/booking.js
@@ -10,4 +10,42 @@ export const checkAuthenticated = async () => {
         console.error ("Error in authentication");
         throw error;
     }
+};
+
+export const createBooking = async (bookingData) => {
+    try {
+        const response = axios.post (`${BASE_URL}/booking`, bookingData, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        console.error ("Error in sending booking request:", error);
+        throw error;
+    }
+};
+
+export const getAndSetBookedMeals = async (username, date) => {
+    try {
+        const response = await axios.get (`${BASE_URL}/booking/getSetMeals`, {
+            params: {
+                username: username,
+                date: date
+            }
+        });
+        console.log ("GetAndSetBookedMeals response.data:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error ("Error in fetching selected meals:", error);
+        throw error;
+    }
+};
+
+export const checkBookedDays = async (username) => {
+    try {
+        const response = await axios.get (`${BASE_URL}/booking/check`, username)
+        console.log ("CheckedBookedDays response.data:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error ("Error in fetching booked days:", error);
+        throw error;
+    }
 }
+
